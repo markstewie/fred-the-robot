76e0ed0a12d556939fb307759f78c29bd8316024
Mark Stewart <mark@subtract.io>
2016-07-18 22:49:15 +1200

    init commit

---
+++ index.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const vorpal = require('vorpal')();
+const position = require('./lib/robot-position');
+const reporter = require('./lib/robot-reporter');
+
+// PLACE X,Y,F (place at xy coords, facing f)
+// MOVE (one step forward in direction facing)
+// LEFT (turns 90 to left)
+// RIGHT (turns 90 to right)
+// REPORT (announce X,Y,F of robot)
+
+vorpal
+  .command('place [xyz]', 'Places me at X,Y facing Z (NSEW).')
+  .action(function(args, callback) {
+    if(position.at(args.xyz)) {
+      reporter.report(position.getPosition());
+    }
+    callback();
+  });
+
+vorpal
+  .command('move', 'Moves me one step forward in facing direction.')
+  .action(function(args, callback) {
+    if(position.move()) {
+      reporter.report(position.getPosition());
+    }
+    callback();
+  });
+
+vorpal
+  .command('left', 'Rotates me to the left 90 deg.')
+  .action(function(args, callback) {
+    if(position.left()) {
+      reporter.report(position.getPosition());
+    }
+    callback();
+  });
+
+vorpal
+  .command('right', 'Rotates me to the right 90 deg.')
+  .action(function(args, callback) {
+    if(position.right()) {
+      reporter.report(position.getPosition());
+    }
+    callback();
+  });
+
+vorpal
+  .command('report', 'Reports on my whereabouts.')
+  .action(function(args, callback) {
+    reporter.report(position.getPosition());
+    callback();
+  });
+
+vorpal
+  .delimiter('fred$')
+  .show();
+
+vorpal.log('Hi there, I\'m Fred the movable robot.\nRun "help" for instructions');
---
+++ readme.md
@@ -0,0 +1,18 @@
+# FRED THE MOVABLE ROBOT!!
+
+###Instructions
+- Clone repo
+- `npm install`
+- `node index.js`
+- Run `help` for instructions
+
+###Notes
+- Fred must be placed first using `place xyz` command with x between 0-4, y between 0-4 & z one of N,S,E or W
+- Move fred with `move` command
+- Rotate using `left` or `right` commands
+- Get a report at any time on Freds position using `report` command (this will be run automatically when he moves);
+
+###Todo's
+- Tests
+- Tidy up
+- Go to bed
